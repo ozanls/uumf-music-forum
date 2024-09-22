356fa4382edbb0a9ad152392c30a6cbd6913bf93
Ozan Lago-Sereflioglu <lago.ozan@gmail.com>
2024-09-21 21:51:17 -0400

    getRandomColor fix; replaced hex's that impair accessibility.

--- server/utilities/GetRandomColor.js
+++ server/utilities/GetRandomColor.js
@@ -1,7 +1,7 @@
 // function for setting a random color
 function getRandomColor() {
 	const hexOptions = [
-		'#ff0000', '#ffa600', '#ffff00', '#008000', '#0000ff', '#4a0080', '#ee81ee'
+		'#ff0000', '#ffa600', '#00ABAB', '#008000', '#0000ff', '#4a0080', '#ee81ee'
 	];
 	const randomHex = Math.floor(Math.random() * hexOptions.length)
 	let color = hexOptions[randomHex];
